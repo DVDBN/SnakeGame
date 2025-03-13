ae1d1acf22bd3b451d32edde4467329044442f4a
dzvoud <dzvoud@gmail.com>
2025-03-13 15:59:37 +0100

    optimise code

--- src/Food.js
+++ src/Food.js
@@ -1,29 +1,25 @@
 class Food {
     constructor(width = 500, height = 500, size = 10) {
         this.size = size;
-        this.body = [
-            {
-                x: this.#randomPosition(width),  // Position x
-                y: this.#randomPosition(height),  // Position y
-            }
-        ];
+        this.regenerate(width, height);
     }
 
-    // Méthode privée pour générer une position valide pour la nourriture (en fonction de width ou height)
+    // Méthode privée pour générer une position valide
     #randomPosition(dimension) {
-        // Calculer une position aléatoire multiple de `size`
-        return Math.floor(Math.random() * (Math.floor(dimension / this.size))) * this.size;
+        return Math.floor(Math.random() * (dimension / this.size)) * this.size;
     }
 
-    // Retourner les coordonnées de la nourriture
+    // Génère une nouvelle position pour la nourriture
+    regenerate(width, height) {
+        this.body = [{ 
+            x: this.#randomPosition(width), 
+            y: this.#randomPosition(height) 
+        }];
+    }
+
+    // Retourner la position de la nourriture
     getBody() {
         return this.body;
-    }
-
-    // Méthode pour régénérer la nourriture avec des coordonnées valides et multiples de 'size'
-    regenerate(width, height) {
-        this.body[0].x = this.#randomPosition(width);  // Générer une nouvelle position x
-        this.body[0].y = this.#randomPosition(height);  // Générer une nouvelle position y
     }
 }
 
